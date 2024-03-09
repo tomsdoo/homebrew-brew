c4d6b8f06b27c1e375d0bc75f9eea2581731feb1
tomsdoo <toms@live.jp>
2024-03-09 16:24:17 +0900

    use-gist@0.3.1

--- Formula/use-gist.rb
+++ Formula/use-gist.rb
@@ -3,9 +3,9 @@ require "language/node"
 class UseGist < Formula
   desc "gist tool"
   homepage "https://github.com/tomsdoo/use-gist"
-  url "https://registry.npmjs.org/@tomsd/use-gist/-/use-gist-0.3.0.tgz"
-  version "0.3.0"
-  sha256 "d1beb25f5bb6e14e9029e607b10e52929635eaef74016af67f02b9bcf4076fbd"
+  url "https://registry.npmjs.org/@tomsd/use-gist/-/use-gist-0.3.1.tgz"
+  version "0.3.1"
+  sha256 "9585bd1f9ac725203e24bbc08b6ee8b4a73a43ee26527af40893f8e8ef36a6d6"
   license "MIT"
   head "https://github.com/tomsdoo/use-gist.git", branch: "main"
 
