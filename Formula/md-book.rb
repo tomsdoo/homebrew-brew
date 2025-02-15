bb262176ead71123a7ad42640374b2450d0789fb
tomsdoo <toms@live.jp>
2025-02-15 13:38:26 +0900

    md-book@1.3.6

--- Formula/md-book.rb
+++ Formula/md-book.rb
@@ -3,9 +3,9 @@ require "language/node"
 class MdBook < Formula
   desc "It helps you to aggregate the markdown resources in one page."
   homepage "https://github.com/tomsdoo/md-book"
-  url "https://registry.npmjs.org/@tomsd/md-book/-/md-book-1.3.5.tgz"
-  version "1.3.5"
-  sha256 "ebee2dfb92e7377667f50559272a6c2ce3fd7c1061edf966c353b28f86f4f963"
+  url "https://registry.npmjs.org/@tomsd/md-book/-/md-book-1.3.6.tgz"
+  version "1.3.6"
+  sha256 "e8d53476eff5bca26963a271209bf976be9e39709dc8aba4aa97b26bb2252bb2"
   license "MIT"
   head "https://github.com/tomsdoo/md-book.git", branch: "main"
 
